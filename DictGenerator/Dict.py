340c7f5fa655ce8259e4658e1320833338c83459
Sabari Rajan <528664@cognizant.com>
2018-10-01 12:08:48 +0530

    Updated DictGenerator

---
+++ DictGenerator/Dict.py
@@ -0,0 +1,26 @@
+"""
+Name        :   Dict
+Usage       :   Used for converting various python objects to Dictionary objects
+Reusability :   Completely Reusable
+Author      :   Sabari Rajan
+Created on  :   08/24/2018
+-----------------------------------------------------------------------------
+Change Log: Recent Modification in TOP
+-----------------------------------------------------------------------------
+Date        Mod By  Ver     Modification
+-----------------------------------------------------------------------------
+08/24/2018  528664  1.00    Initial Code
+"""
+
+
+class Dict:
+
+    def __init__(self, arg):
+        return None
+
+
+if __name__ == "__main__":
+    string_value = ""
+    list_value = []
+    object_value = {}
+    temp_object = Dict()
